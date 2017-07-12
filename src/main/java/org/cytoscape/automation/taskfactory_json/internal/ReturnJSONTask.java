7719b561495e7f939326053d1aa55c98f8de2374
David Otasek <dotasek.dev@gmail.com>
2017-07-12 13:27:56 -0400

    Updated to Match Task/JSON API Changes

--- src/main/java/org/cytoscape/automation/taskfactory_json/internal/ReturnJSONTask.java
+++ src/main/java/org/cytoscape/automation/taskfactory_json/internal/ReturnJSONTask.java
@@ -1,6 +1,8 @@
 package org.cytoscape.automation.taskfactory_json.internal;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.ObservableTask;
@@ -35,7 +37,6 @@ public class ReturnJSONTask extends AbstractTask implements ObservableTask {
 	
 	@SuppressWarnings("unchecked")
 	@Override
-	@ResultClasses(classes={String.class, SampleResult.class, SampleJSONResult.class})
 	public <R> R getResults(Class<? extends R> type) {
 		if (type.equals(String.class)) {
 			return (R) new SampleJSONResult(result).getJSON();
@@ -48,4 +49,8 @@ public class ReturnJSONTask extends AbstractTask implements ObservableTask {
 		}
 	}
 
+	@Override 
+	public List<Class<?>> getResultClasses() {
+		return Arrays.asList(String.class, SampleJSONResult.class);
+	}
 }
