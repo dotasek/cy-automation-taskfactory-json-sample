d3aca78b90a52a2f80a0d74dffcc4a88d6c1a363
David Otasek <dotasek.dev@gmail.com>
2017-08-31 18:41:13 -0400

    Added code to demonstrate possible new JSON handling

--- src/main/java/org/cytoscape/automation/taskfactory_json/internal/ReturnJSONTask.java
+++ src/main/java/org/cytoscape/automation/taskfactory_json/internal/ReturnJSONTask.java
@@ -9,6 +9,9 @@ import org.cytoscape.work.ObservableTask;
 import org.cytoscape.work.ProvidesTitle;
 import org.cytoscape.work.TaskMonitor;
 import org.cytoscape.work.Tunable;
+import org.cytoscape.work.json.JSONResult;
+
+import com.google.gson.Gson;
 
 public class ReturnJSONTask extends AbstractTask implements ObservableTask {
 	
@@ -19,7 +22,7 @@ public class ReturnJSONTask extends AbstractTask implements ObservableTask {
 	@ProvidesTitle
 	public String getTitle() { return "TaskFactory Return JSON Sample"; }
 
-	@Tunable (description="Name", longDescription="The name to pass in the name field")
+	@Tunable (description="Name", longDescription="The value to return in the result's ```name``` field", exampleStringValue="Mort")
 	public String name = "";
 	
 	private SampleResult result;
@@ -27,6 +30,12 @@ public class ReturnJSONTask extends AbstractTask implements ObservableTask {
 	@Override
 	public void run(TaskMonitor arg0) throws Exception {
 		result = new SampleResult();
+		
+		if (name == null || name.length() == 0) {
+			Exception e = new Exception("Name parameter cannot be empty.");
+			throw e;
+		}
+		
 		result.name = name;
 		result.values = new ArrayList<Integer>();
 		result.values.add(1);
@@ -38,12 +47,15 @@ public class ReturnJSONTask extends AbstractTask implements ObservableTask {
 	@Override
 	public <R> R getResults(Class<? extends R> type) {
 		if (type.equals(String.class)) {
-			return (R) new SampleJSONResult(result).getJSON();
+			String json = (new Gson()).toJson(result);
+			
+			return (R)(new Gson()).toJson(result);
 		} 
 		/* This is where we return JSON from this Task. 
 		 */
-		else if (type.equals(SampleJSONResult.class)) {
-			return (R) new SampleJSONResult(result);
+		else if (type.equals(JSONResult.class)) {
+			JSONResult res = () -> {return (new Gson()).toJson(result);};
+			return (R)(res);
 		} else {
 			return null;
 		}
@@ -51,6 +63,6 @@ public class ReturnJSONTask extends AbstractTask implements ObservableTask {
 
 	@Override 
 	public List<Class<?>> getResultClasses() {
-		return Arrays.asList(String.class, SampleJSONResult.class);
+		return Arrays.asList(String.class, JSONResult.class);
 	}
 }
